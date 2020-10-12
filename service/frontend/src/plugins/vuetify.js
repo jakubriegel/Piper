20b7660b09796f2eee67a7eb64425b245f020135
Wojciech Kasperski <wojciech.kasperski97@gmail.com>
2020-10-12 18:40:11 +0200

    Inital Template

--- service/frontend/src/plugins/vuetify.js
+++ service/frontend/src/plugins/vuetify.js
@@ -3,4 +3,24 @@ import Vuetify from "vuetify/lib";
 
 Vue.use(Vuetify);
 
-export default new Vuetify({});
+export default new Vuetify({
+  theme: {
+    options: {
+      customProperties: true,
+    },
+    themes: {
+      light: {
+        primary: "#212121",
+        secondary: "#212121",
+        accent: "#1DB954",
+        error: "#FF5252",
+        info: "#2196F3",
+        success: "#4CAF50",
+        warning: "#FFC107",
+      },
+    },
+  },
+  icons: {
+    iconfont: "mdi",
+  },
+});
--- service/frontend/src/store/index.js
+++ service/frontend/src/store/index.js
@@ -4,8 +4,29 @@ import Vuex from "vuex";
 Vue.use(Vuex);
 
 export default new Vuex.Store({
-  state: {},
-  mutations: {},
+  state: {
+    routes: [
+      {
+        text: "Dashboard",
+        to: "/"
+      },
+      {
+        text: "Search",
+        to: "/search"
+      },
+      {
+        text: "About",
+        to: "/about"
+      }
+    ]
+  },
+  getters: {
+    routes: state => state.routes
+  },
+  mutations: {
+    setDrawer: (state, payload) => (state.drawer = payload),
+    toggleDrawer: state => (state.drawer = !state.drawer)
+  },
   actions: {},
   modules: {}
 });
