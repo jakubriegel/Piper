9af0e52406ccc63c39da266ef59da6b51399b788
jakubriegel <jakubriegel112@gmail.com>
2020-10-21 16:13:36 +0200

    home-service | rooms endpoints cors

--- service/home-service/src/main/kotlin/eu/jrie/put/piper/piperhomeservice/api/HousesController.kt
+++ service/home-service/src/main/kotlin/eu/jrie/put/piper/piperhomeservice/api/HousesController.kt
@@ -13,12 +13,14 @@ import org.springframework.http.MediaType.APPLICATION_JSON_VALUE
 import org.springframework.http.ResponseEntity
 import org.springframework.http.ResponseEntity.ok
 import org.springframework.security.core.Authentication
+import org.springframework.web.bind.annotation.CrossOrigin
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 import reactor.core.publisher.Mono
 
+@CrossOrigin
 @RestController
 @RequestMapping("houses")
 class HousesController (
