7b534811948e203371dca09fe7890cc413fa9ae6
Evenlaxxus <r.ewiak1404@wp.pl>
2020-10-28 13:24:36 +0100

    Changes routine list to data table

--- service/frontend/src/views/Routines.vue
+++ service/frontend/src/views/Routines.vue
@@ -1,43 +1,55 @@
 <template>
-  <v-row no-gutters>
-    <v-col class="pa-3" cols="3" v-for="routine in routines" :key="routine.id">
-      <Routine :sequence="routine" />
-    </v-col>
-    <v-col class="pa-3" cols="3">
-      <v-hover v-slot="{ hover }">
-        <v-card
-          class="d-flex align-content-center justify-center flex-wrap"
-          outlined
-          height="100%"
-          :elevation="hover ? 16 : 2"
-          :class="{ 'on-hover': hover }"
-          @click="addEmptyRoutine"
-        >
-          <v-icon size="50">mdi-plus-box</v-icon>
-        </v-card>
-      </v-hover>
-    </v-col>
-  </v-row>
+  <v-container>
+    <v-card>
+      <v-data-table :headers="headers" :items="routines">
+        <template v-slot:item.enabled="{ item }">
+          <v-checkbox v-model="item.enabled"></v-checkbox>
+        </template>
+        <template v-slot:item.actions="{ item }">
+          <v-icon
+            small
+            class="mr-2"
+            @click="$router.push('/routine/' + item.id)"
+          >
+            mdi-pencil
+          </v-icon>
+          <v-icon small @click="deleteItem(item.id)">
+            mdi-delete
+          </v-icon>
+        </template>
+      </v-data-table>
+    </v-card>
+  </v-container>
 </template>
 
 <script>
-import Routine from "@/components/Routine";
-import { mapGetters, mapActions } from "vuex";
+import { mapGetters } from "vuex";
 export default {
   name: "CustomRoutines",
-  components: { Routine },
   computed: {
     ...mapGetters("routines", ["routines"])
   },
+
+  data: () => ({
+    headers: [
+      {
+        text: "Id",
+        align: "start",
+        sortable: false,
+        value: "id"
+      },
+      { text: "Name", value: "name" },
+      { text: "Enabled", value: "enabled" },
+      { text: "Actions", value: "actions" }
+    ]
+  }),
+
   methods: {
-    ...mapActions("routines", ["addEmptyRoutine"])
+    deleteItem(item) {
+      console.log("Delete " + item);
+    }
   }
 };
 </script>
 
-<style lang="sass" scoped>
-.v-card.on-hover
-  background-color: rgba(#FFF, 0.8)
-  >.v-icon
-    color: #2196f3
-</style>
+<style lang="sass" scoped></style>
