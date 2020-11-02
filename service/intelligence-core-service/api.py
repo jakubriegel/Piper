b04246773b9eaee6a72f355cfd9013fe17abb2e4
Michał Kalinowski <44393752+KalinZeglarz@users.noreply.github.com>
2020-11-02 14:20:09 +0100

    Status codes errorhandler

--- service/intelligence-core-service/api.py
+++ service/intelligence-core-service/api.py
@@ -1,4 +1,5 @@
 from flask import Flask, request, Response
+from werkzeug.exceptions import HTTPException, abort
 import json
 
 from modelService import ModelService
@@ -8,12 +9,30 @@ app = Flask(__name__)
 modelServiceInstance = ModelService()
 
 
+@app.errorhandler(Exception)
+def handle_error(e):
+    code = 404
+    if isinstance(e, HTTPException):
+        code = e.code
+        if e.code == 422:
+            return Response(response=json.dumps('UNABLE_TO_PROCESS'), status=code, mimetype='application/json')
+        elif e.code == 500:
+            return Response(response=json.dumps('INTERNAL_SERVER_ERROR'), status=code, mimetype='application/json')
+        else:
+            return Response(response=json.dumps('PAGE_NOT_FOUND'), status=code, mimetype='application/json')
+
+
+@app.route('/', methods=['GET'])
+def index():
+    abort(500)
+
+
 @app.route('/status', methods=['GET'])
 def get_status():
     try:
         return Response(response=json.dumps('active'), status=200, mimetype='application/json')
     except:
-        return Response(status=500)
+        abort(500)
 
 
 @app.route('/get-sequence', methods=['GET'])
@@ -23,9 +42,11 @@ def get_predictions():
         event = request.args.get('event')
         limit = int(request.args.get('limit'))
         prediction = modelServiceInstance.predict(modelId, event, limit)
+        if len(prediction) <= 0:
+            return Response(response=json.dumps('no content'), status=204, mimetype='application/json')
         return Response(response=json.dumps({"sequence": prediction}), status=200, mimetype='application/json')
     except:
-        return Response(status=422)
+        abort(422)
 
 
 if __name__ == '__main__':
