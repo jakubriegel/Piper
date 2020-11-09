d39b820d6c1199b6d9484b7fb4462ec688428e29
Michał Kalinowski <44393752+KalinZeglarz@users.noreply.github.com>
2020-11-09 21:09:36 +0100

    Simple client creation

---
+++ service/sample-automation-client/client.py
@@ -0,0 +1,28 @@
+import time
+
+import requests
+import uuid
+
+url = "https://jrie.eu:8001/events"
+while True:
+    payload = str(int(time.time())) + "," + str(uuid.uuid4()) + "," + str(uuid.uuid4()) + "\r\n" + \
+              str(int(time.time())) + "," + str(uuid.uuid4()) + "," + str(uuid.uuid4()) + "\r\n" + \
+              str(int(time.time())) + "," + str(uuid.uuid4()) + "," + str(uuid.uuid4()) + "\r\n" + \
+              str(int(time.time())) + "," + str(uuid.uuid4()) + "," + str(uuid.uuid4()) + "\r\n" + \
+              str(int(time.time())) + "," + str(uuid.uuid4()) + "," + str(uuid.uuid4()) + "\r\n" + \
+              str(int(time.time())) + "," + str(uuid.uuid4()) + "," + str(uuid.uuid4()) + "\r\n" + \
+              str(int(time.time())) + "," + str(uuid.uuid4()) + "," + str(uuid.uuid4()) + "\r\n" + \
+              str(int(time.time())) + "," + str(uuid.uuid4()) + "," + str(uuid.uuid4()) + "\r\n" + \
+              str(int(time.time())) + "," + str(uuid.uuid4()) + "," + str(uuid.uuid4()) + "\r\n" + \
+              str(int(time.time())) + "," + str(uuid.uuid4()) + "," + str(uuid.uuid4())
+    print(payload)
+    headers = {
+        'Content-Type': 'text/csv',
+        'Authorization': 'Basic aG91c2UtMS1zZXJ2ZXI6c2VjcmV0'
+    }
+
+    response = requests.request("POST", url, headers=headers, data=payload, verify=False)
+
+    print(response.text)
+    print(response.status_code)
+    time.sleep(30)
