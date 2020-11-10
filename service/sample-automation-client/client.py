0983d522275af6a44deb5f24faa2543faee7d6c4
Michał Kalinowski <44393752+KalinZeglarz@users.noreply.github.com>
2020-11-10 20:19:28 +0100

    first-contact function creation
    getting devices from .json file, sending PUT

--- service/sample-automation-client/client.py
+++ service/sample-automation-client/client.py
@@ -1,29 +1,52 @@
+import json
 import time
-
 import requests
 import uuid
 
+class Client:
+    def __init__(self):
 
-url = "https://jrie.eu:8001/events"
-while True:
-    payload = str(int(time.time())) + "," + str(uuid.uuid4()) + "," + str(uuid.uuid4()) + "\r\n" + \
-              str(int(time.time())) + "," + str(uuid.uuid4()) + "," + str(uuid.uuid4()) + "\r\n" + \
-              str(int(time.time())) + "," + str(uuid.uuid4()) + "," + str(uuid.uuid4()) + "\r\n" + \
-              str(int(time.time())) + "," + str(uuid.uuid4()) + "," + str(uuid.uuid4()) + "\r\n" + \
-              str(int(time.time())) + "," + str(uuid.uuid4()) + "," + str(uuid.uuid4()) + "\r\n" + \
-              str(int(time.time())) + "," + str(uuid.uuid4()) + "," + str(uuid.uuid4()) + "\r\n" + \
-              str(int(time.time())) + "," + str(uuid.uuid4()) + "," + str(uuid.uuid4()) + "\r\n" + \
-              str(int(time.time())) + "," + str(uuid.uuid4()) + "," + str(uuid.uuid4()) + "\r\n" + \
-              str(int(time.time())) + "," + str(uuid.uuid4()) + "," + str(uuid.uuid4()) + "\r\n" + \
-              str(int(time.time())) + "," + str(uuid.uuid4()) + "," + str(uuid.uuid4())
+
+def first_contact():
+    url = "https://jrie.eu:8001/houses/schema"
+    payload = json.dumps(json.load(open("devices.json")))
     print(payload)
     headers = {
-        'Content-Type': 'text/csv',
-        'Authorization': 'Basic aG91c2UtMS1zZXJ2ZXI6c2VjcmV0'
+      'Accept': 'application/json',
+      'Origin': 'localhost',
+      'Authorization': 'Basic aG91c2UtMi1zZXJ2ZXI6c2VjcmV0',
+      'Content-Type': 'application/json'
     }
 
-    response = requests.request("POST", url, headers=headers, data=payload, verify=False)
+    response = requests.request("PUT", url, headers=headers, data=payload, verify=False)
 
     print(response.text)
-    print(response.status_code)
-    time.sleep(30)
+
+def send_data():
+    url = "https://jrie.eu:8001/events"
+    while True:
+        payload = str(int(time.time())) + "," + str(uuid.uuid4()) + "," + str(uuid.uuid4()) + "\r\n" + \
+                  str(int(time.time())) + "," + str(uuid.uuid4()) + "," + str(uuid.uuid4()) + "\r\n" + \
+                  str(int(time.time())) + "," + str(uuid.uuid4()) + "," + str(uuid.uuid4()) + "\r\n" + \
+                  str(int(time.time())) + "," + str(uuid.uuid4()) + "," + str(uuid.uuid4()) + "\r\n" + \
+                  str(int(time.time())) + "," + str(uuid.uuid4()) + "," + str(uuid.uuid4()) + "\r\n" + \
+                  str(int(time.time())) + "," + str(uuid.uuid4()) + "," + str(uuid.uuid4()) + "\r\n" + \
+                  str(int(time.time())) + "," + str(uuid.uuid4()) + "," + str(uuid.uuid4()) + "\r\n" + \
+                  str(int(time.time())) + "," + str(uuid.uuid4()) + "," + str(uuid.uuid4()) + "\r\n" + \
+                  str(int(time.time())) + "," + str(uuid.uuid4()) + "," + str(uuid.uuid4()) + "\r\n" + \
+                  str(int(time.time())) + "," + str(uuid.uuid4()) + "," + str(uuid.uuid4())
+        print(payload)
+        headers = {
+            'Content-Type': 'text/csv',
+            'Authorization': 'Basic aG91c2UtMi1zZXJ2ZXI6c2VjcmV0'
+        }
+        response = requests.request("POST", url, headers=headers, data=payload, verify=False)
+        print(response.text)
+        print(response.status_code)
+
+
+if __name__ == '__main__':
+    first_contact()
+    # while True:
+    #     send_data()
+    #     time.sleep(30)
