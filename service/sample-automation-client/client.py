317ae1333e53f1f5b3e855708639ff81a65f9b20
Michał Kalinowski <44393752+KalinZeglarz@users.noreply.github.com>
2020-11-12 20:04:17 +0100

    console arguments added
    use --reset to ask for ids first

--- service/sample-automation-client/client.py
+++ service/sample-automation-client/client.py
@@ -1,19 +1,24 @@
 import json
 import time
 import random
+from pathlib import Path
 
 import requests
-import uuid
+import sys
 
 
 class SampleClient:
     devIds_with_types: dict = {}
     typeIds_with_actions: dict = {}
 
+    def load_devices(self):
+
+        self.devIds_with_types = json.load(open("devIds_with_types.json"))
+        self.typeIds_with_actions = json.load(open("typeIds_with_actions.json"))
+
     def first_contact(self):
         url = "https://jrie.eu:8001/houses/schema"
         payload = json.dumps(json.load(open("devices.json")))
-        print(payload)
         headers = {
             'Accept': 'application/json',
             'Authorization': 'Basic aG91c2UtMi1zZXJ2ZXI6c2VjcmV0',
@@ -31,6 +36,12 @@ class SampleClient:
             for device in room['devices']:
                 self.devIds_with_types[device['id']] = device['typeId']
 
+        with open('typeIds_with_actions.json', 'w') as file1:
+            file1.write(json.dumps(self.typeIds_with_actions))
+
+        with open('devIds_with_types.json', 'w') as file2:
+            file2.write(json.dumps(self.devIds_with_types))
+
     def send_data(self):
         url = "https://jrie.eu:8001/events"
         payload = ''
@@ -38,7 +49,7 @@ class SampleClient:
             device, devType = random.choice(list(self.devIds_with_types.items()))
             event = random.choice(self.typeIds_with_actions[devType])
             payload += str(int(time.time())) + "," + device + "," + event + "\r\n"
-        print(payload)
+            time.sleep(random.randint(1, 300))  # change here to generate faster
         headers = {
             'Content-Type': 'text/csv',
             'Authorization': 'Basic aG91c2UtMi1zZXJ2ZXI6c2VjcmV0'
@@ -50,7 +61,12 @@ class SampleClient:
 
 if __name__ == '__main__':
     client = SampleClient()
-    client.first_contact()
+    if len(sys.argv) > 1 and sys.argv[1] == '--reset':
+        client.first_contact()
+    else:
+        if Path('devIds_with_types.json').is_file() and Path('typeIds_with_actions.json').is_file():
+            client.load_devices()
+        else:
+            client.first_contact()
     while True:
         client.send_data()
-        time.sleep(random.randint(30, 3600))
