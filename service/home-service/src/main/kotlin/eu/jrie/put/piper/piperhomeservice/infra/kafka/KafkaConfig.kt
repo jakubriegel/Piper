1a9690a2a02f17e6aeeb3d9e0df4aff4aaaeca40
Jakub Riegel <jakub.riegel@allegro.pl>
2020-11-15 13:09:28 +0100

    home-service | kafka config for deployment

--- service/home-service/src/main/kotlin/eu/jrie/put/piper/piperhomeservice/infra/kafka/KafkaConfig.kt
+++ service/home-service/src/main/kotlin/eu/jrie/put/piper/piperhomeservice/infra/kafka/KafkaConfig.kt
@@ -1,30 +1,32 @@
 package eu.jrie.put.piper.piperhomeservice.infra.kafka
 
 import eu.jrie.put.piper.piperhomeservice.domain.model.NewModelEvent
-import org.apache.kafka.clients.producer.ProducerConfig.ACKS_CONFIG
-import org.apache.kafka.clients.producer.ProducerConfig.BOOTSTRAP_SERVERS_CONFIG
-import org.apache.kafka.clients.producer.ProducerConfig.CLIENT_ID_CONFIG
 import org.apache.kafka.clients.producer.ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG
 import org.apache.kafka.clients.producer.ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG
+import org.springframework.boot.autoconfigure.kafka.KafkaProperties
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.kafka.core.reactive.ReactiveKafkaProducerTemplate
 import org.springframework.kafka.support.serializer.JsonSerializer
+import reactor.kafka.sender.SenderOptions
 import reactor.kafka.sender.SenderOptions.create
 
 @Configuration
 class KafkaConfig {
 
-    private fun producerOptions() = mapOf(
-            BOOTSTRAP_SERVERS_CONFIG to "localhost:9092",
-            CLIENT_ID_CONFIG to "home-service",
-            ACKS_CONFIG to "all",
-            KEY_SERIALIZER_CLASS_CONFIG to JsonSerializer::class.java,
-            VALUE_SERIALIZER_CLASS_CONFIG to JsonSerializer::class.java,
-    )
+    @Bean
+    fun producerOptions(properties: KafkaProperties): SenderOptions<Int, NewModelEvent>
+            = create((properties.buildProducerProperties() + additionalOptions))
 
     @Bean
-    fun  modelMessageProducer(): ReactiveKafkaProducerTemplate<Int, NewModelEvent> {
-        return ReactiveKafkaProducerTemplate(create(producerOptions()))
+    fun  modelMessageProducer(options: SenderOptions<Int, NewModelEvent>): ReactiveKafkaProducerTemplate<Int, NewModelEvent> {
+        return ReactiveKafkaProducerTemplate(options)
+    }
+
+    private companion object {
+        val additionalOptions: Map<String, Any> = mapOf(
+                KEY_SERIALIZER_CLASS_CONFIG to JsonSerializer::class.java,
+                VALUE_SERIALIZER_CLASS_CONFIG to JsonSerializer::class.java
+        )
     }
 }
