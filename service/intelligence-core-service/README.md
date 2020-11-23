63adcf3accdb0a4e94fc9258930c9399fb25f85a
Wojciech Kasperski <wojciech.kasperski97@gmail.com>
2020-11-23 14:40:52 +0100

    get-predictions now returns real predictions

--- service/intelligence-core-service/README.md
+++ service/intelligence-core-service/README.md
@@ -1,2 +1,17 @@
 # Piper intelligence-core-service
 Piper will be an AI based system, that will be able to propose automated routines suggestions based on real life events.
+
+## Deployment
+
+### Local
+Create `venv` environment and install dependencies by running these commands: 
+```
+python -m venv venv
+source venv/bin/activate
+pip install -r requirements.txt
+```
+### Docker
+Just run docker container :) 
+```
+docker-compose -d intelligence-core-service
+```
--- service/intelligence-core-service/app/serveModel.py
+++ service/intelligence-core-service/app/serveModel.py
@@ -1,29 +1,51 @@
 import json
-
+import os
+from typing import List
 import tensorflow as tf
+
+MODELS_DIR = '/models'
+# change it to /models on Docker and models locally (make copy model to models folder and
+# change it's name f.eg. to 123_model where 123 is modelId)
 
 
 class ServeModel:
     def __init__(self):
-        self.models = {
-            0: tf.keras.models.load_model('model', compile=True)
-        }
-        self.categories_dict = self.load_categories_dict('model/category_dict.json')
+        self.modelId = ''
+        self.models = []
+        self.categories_dict = {}
+        self.model = None
+        self.__scan_models_directory(MODELS_DIR)
 
-    def load_categories_dict(self, path):
-        with open(path) as f:
-            return json.load(f)
+    def __scan_models_directory(self, path: str):
+        models_list = os.scandir(path=path)
+        tmp_models_list = []
 
-    def get_category_by_id(self, category_id):
+        print('Available models:')
+
+        for model in models_list:
+            if model.is_dir():
+                tmp_models_list.append(model.name)
+                print(model.name)
+
+        self.models = tmp_models_list
+
+    def load_model(self, model_id: str) -> None:
+        self.modelId = model_id
+        self.categories_dict = load_dict(f'{MODELS_DIR}/{model_id}_model/category_dict.json')
+        self.model = tf.keras.models.load_model(f'{MODELS_DIR}/{model_id}_model', compile=True)
+
+    def get_category_by_id(self, category_id: int):
         return self.categories_dict[str(category_id)]
 
-    def get_category_by_name(self, category_name):
+    def get_category_by_name(self, category_name: str) -> int:
         key_list = list(self.categories_dict.keys())
         val_list = list(self.categories_dict.values())
         return int(key_list[val_list.index(category_name)])
 
-    def generate_sequences(self, model_id, initial_event_name, num_generate=10):
-        model = self.models[model_id]
+    def generate_sequences(self, model_id: str, initial_event_name: str, num_generate: int = 10) -> List[str]:
+        self.load_model(model_id)
+        print(f'Making prediction using model: {model_id}, start event: {initial_event_name} and n={num_generate}')
+
         start_sequence_event_id = self.get_category_by_name(initial_event_name)
 
         # Converting our start frame to vector of numbers (vectorizing)
@@ -39,9 +61,9 @@ class ServeModel:
         temperature = 1.0
 
         # Here batch size == 1
-        model.reset_states()
+        self.model.reset_states()
         for i in range(num_generate):
-            predictions = model(input_eval)
+            predictions = self.model(input_eval)
             # remove the batch dimension
             predictions = tf.squeeze(predictions, 0)
 
@@ -57,3 +79,13 @@ class ServeModel:
 
         generated_sequences.insert(0, self.get_category_by_id(start_sequence_event_id))
         return generated_sequences
+
+
+def load_dict(path: str):
+    with open(path) as f:
+        return json.load(f)
+
+
+# if __name__ == '__main__':
+#     serve_model = ServeModel()
+#     print(serve_model.generate_sequences('31231231231-3123-1231323-1321231','bathroom_light_1_switch_light_off', 5))
