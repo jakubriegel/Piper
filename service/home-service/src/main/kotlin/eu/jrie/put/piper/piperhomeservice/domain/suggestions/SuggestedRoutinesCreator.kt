6fbf302d76c1979e24dcb5f3e88cfc84f20a353f
jakubriegel <jakubriegel112@gmail.com>
2021-01-10 18:18:39 +0100

    home-service | delete old suggestions

--- service/home-service/src/main/kotlin/eu/jrie/put/piper/piperhomeservice/domain/suggestions/SuggestedRoutinesCreator.kt
+++ service/home-service/src/main/kotlin/eu/jrie/put/piper/piperhomeservice/domain/suggestions/SuggestedRoutinesCreator.kt
@@ -12,9 +12,12 @@ import kotlinx.coroutines.FlowPreview
 import kotlinx.coroutines.flow.map
 import kotlinx.coroutines.flow.toList
 import kotlinx.coroutines.reactor.asFlux
+import org.slf4j.Logger
+import org.slf4j.LoggerFactory
 import org.springframework.stereotype.Component
 import reactor.core.publisher.Mono
 import reactor.core.publisher.Mono.empty
+import reactor.core.publisher.Mono.zip
 import reactor.kotlin.core.publisher.toFlux
 
 @FlowPreview
@@ -25,28 +28,37 @@ class SuggestedRoutinesCreator (
         private val suggestionsService: SuggestionsService
 ) {
     fun createSuggestedRoutines(houseId: String): Mono<Void> {
+        logger.info("Creating suggested routines for house: $houseId")
         val user = User(nextUUID, "", "", houseId, emptySet())
         val devicesMono = housesService.getDevices(user).asFlux().collectList()
         val typesMono = housesService.getDevicesTypes(user)
                 .map { (type, events) -> type.id to events.toList() }
                 .asFlux().collectList()
-        return Mono.zip(devicesMono, typesMono)
+        return repository.deleteAllByHouseId(houseId)
+            .then(zip(devicesMono, typesMono))
             .map { (devices, types) -> devices to mapOf(*types.toTypedArray()) }
             .flatMapMany { (devices, events) -> List(15) { randomEvent(devices, events) }.toFlux() }
             .distinct()
             .flatMap { head ->
                 val n = (2..5).random()
-                suggestionsService.getContinuationSuggestions(head, n, user).asFlux().collectList()
+                suggestionsService.getContinuationSuggestions(head, n, user)
+                    .asFlux().collectList()
+                    .onErrorResume { empty() }
             }
             .map { items -> SuggestedRoutine(nextUUID, houseId, items) }
+            .map { logger.info("Created routine $it"); it }
             .flatMap { repository.insert(it) }
-            .flatMap { empty<Void>() }
-            .reduce { a, _ -> a }
+            .collectList()
+            .then(empty())
     }
 
-    private fun randomEvent(devices: List<Device>, events: Map<String, List<DeviceEvent>>): RoutineEvent {
-        val device = devices.random()
-        val event = events.getValue(device.typeId).random()
-        return RoutineEvent(device.id, event.id)
+    private companion object {
+        fun randomEvent(devices: List<Device>, events: Map<String, List<DeviceEvent>>): RoutineEvent {
+            val device = devices.random()
+            val event = events.getValue(device.typeId).random()
+            return RoutineEvent(device.id, event.id)
+        }
+
+        val logger: Logger = LoggerFactory.getLogger(SuggestedRoutinesCreator::class.java)
     }
 }
--- service/home-service/src/main/kotlin/eu/jrie/put/piper/piperhomeservice/domain/suggestions/suggestedRoutine.kt
+++ service/home-service/src/main/kotlin/eu/jrie/put/piper/piperhomeservice/domain/suggestions/suggestedRoutine.kt
@@ -2,18 +2,17 @@ package eu.jrie.put.piper.piperhomeservice.domain.suggestions
 
 import eu.jrie.put.piper.piperhomeservice.domain.routine.RoutineEvent
 import eu.jrie.put.piper.piperhomeservice.infra.repository.CustomRepository
-import org.bson.types.ObjectId
 import org.springframework.data.annotation.Id
 import org.springframework.data.mongodb.core.ReactiveMongoTemplate
 import org.springframework.data.mongodb.core.aggregation.Aggregation.match
 import org.springframework.data.mongodb.core.aggregation.Aggregation.newAggregation
 import org.springframework.data.mongodb.core.aggregation.Aggregation.sample
 import org.springframework.data.mongodb.core.mapping.Document
-import org.springframework.data.mongodb.core.query.Criteria
 import org.springframework.data.mongodb.core.query.Criteria.where
 import org.springframework.data.mongodb.repository.ReactiveMongoRepository
 import org.springframework.stereotype.Repository
 import reactor.core.publisher.Flux
+import reactor.core.publisher.Mono
 
 
 @Document
@@ -24,7 +23,9 @@ data class SuggestedRoutine(
         val events: List<RoutineEvent>
 )
 
-interface SuggestedRoutinesRepository : ReactiveMongoRepository<SuggestedRoutine, String>, SuggestedRoutinesRepositoryCustom
+interface SuggestedRoutinesRepository : ReactiveMongoRepository<SuggestedRoutine, String>, SuggestedRoutinesRepositoryCustom {
+    fun deleteAllByHouseId(houseId: String): Mono<Void>
+}
 
 interface SuggestedRoutinesRepositoryCustom {
     fun findRandom(n: Int, houseId: String): Flux<SuggestedRoutine>
