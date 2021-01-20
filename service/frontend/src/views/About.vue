9fba53f9d733ffd6d87ea7b0a730ef6f403ebf88
Wojciech Kasperski <wojciech.kasperski97@gmail.com>
2021-01-20 12:16:21 +0100

    Add abstract to about

--- service/frontend/src/views/About.vue
+++ service/frontend/src/views/About.vue
@@ -5,12 +5,27 @@
         <div class="about">
           <h1>About Piper</h1>
           <p>
+            Piper is a machine learning based system, that is able enhance smart
+            homes with auto-mated creation of routines based on real life
+            events. This work presents key aspects of the system, describes its
+            development process and provides guides for prospective integrators
+            and users. First chapter discusses motivation for the project and
+            defines its purpose. The next one depicts architectural concepts
+            used in the system. The third chapter documents characteristics and
+            implementation of Piper’s modules. It is followed by the section
+            about deploying the system. Next two chapters are guides for
+            integrators and end users. They are followed by report from tests of
+            the system. The paper is ended with conclusions designing,
+            developing and testing Piper.
+          </p>
+          <p>
             Piper is project for engineering thesis written on Poznan University
             of Technology, by
             <i>Wojciech Kasperski, Jakub Riegiel, Rafał Ewiak </i>and<i>
               Michał Kalinowski </i
-            >. Project was supervised by <i>dr inż. Marek Kraft.</i>
+            >.
           </p>
+          <p>Project was supervised by <i>dr inż. Marek Kraft.</i></p>
         </div>
       </v-col>
     </v-row>
