6c821433024ead534011d4228fa27e429f6cc8f1
Wojciech Kasperski <wojciech.kasperski97@gmail.com>
2021-01-21 19:39:25 +0100

    Add Avatars

--- service/frontend/src/views/About.vue
+++ service/frontend/src/views/About.vue
@@ -3,8 +3,8 @@
     <v-row>
       <v-col cols="12">
         <div class="about">
-          <h1>About Piper</h1>
-          <p>
+          <h1 class="text-center my-3">About Piper</h1>
+          <p class="text-justify">
             Piper is a machine learning based system, that is able enhance smart
             homes with auto-mated creation of routines based on real life
             events. This work presents key aspects of the system, describes its
@@ -28,6 +28,51 @@
           <p>Project was supervised by <i>dr inż. Marek Kraft.</i></p>
         </div>
       </v-col>
+      <v-col class="mb-5" cols="12">
+        <h2 class="headline font-weight-bold mb-3 text-center">
+          Authors
+        </h2>
+
+        <v-row justify="center">
+          <v-col
+            cols="12"
+            sm="6"
+            md="3"
+            v-for="(next, i) in authors"
+            :key="i"
+            class="text-center pa-10"
+          >
+            <v-img :src="next.src" :alt="next.name" class="rounded-circle" />
+            <h3 class="mt-5">{{ next.name }}</h3>
+          </v-col>
+        </v-row>
+      </v-col>
     </v-row>
   </v-container>
 </template>
+
+<script>
+export default {
+  name: 'Home',
+  data: () => ({
+    authors: [
+      {
+        name: 'Wojciech Kasperski',
+        src: '/authors-img/wk-avatar.png'
+      },
+      {
+        name: 'Jakub Riegel',
+        src: '/authors-img/jr-avatar.jpeg'
+      },
+      {
+        name: 'Michał Kalinowski',
+        src: '/authors-img/mk-avatar.jpg'
+      },
+      {
+        name: 'Rafał Ewiak',
+        src: '/authors-img/re-avatar.jpg'
+      }
+    ]
+  })
+};
+</script>
--- service/frontend/src/views/Home.vue
+++ service/frontend/src/views/Home.vue
@@ -12,7 +12,7 @@
 
       <v-col class="mb-4">
         <h1 class="display-2 font-weight-bold mb-3">
-          Automate Home Automation
+          Automate Your Routines
         </h1>
 
         <p class="subheading font-weight-regular">
